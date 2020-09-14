8e0987fb01c628e1c537030a7a2ea43fd3fced26
ZaidOdeh <odehzaid140@gmail.com>
2020-09-14 14:52:50 +0300

    completed main file, next to do is page1, page2

--- fair/lib/main.dart
+++ fair/lib/main.dart
@@ -1,117 +1,309 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-
+import 'package:splashscreen/splashscreen.dart';
+import 'package:glitters/glitters.dart';
+import 'package:image_picker_gallery_camera/image_picker_gallery_camera.dart';
 void main() {
-  runApp(MyApp());
+  runApp(splash());
 }
+class splash extends StatefulWidget {
+  @override
+  _splashState createState() => _splashState();
+}
+class _splashState extends State<splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+    setState(() {});
+  }
+   @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+     return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: WillPopScope(
+            onWillPop: () => _onBackPressed(),
+            child: Stack(
+
+              children: [
+                SplashScreen(
+                    seconds: 4,
+                    navigateAfterSeconds: Fair(),
+                    image: Image.asset(
+                      "assets/logo2.png",
+                    ),
+                    backgroundColor: Color.fromRGBO(238, 238, 238, 1),
+                    photoSize: 180,
+                    loadingText: Text(
+                      '',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    loaderColor: Colors.transparent),
+                Glitters(
+                  minSize: 8.0,
+                  maxSize: 20.0,
+                  interval: Duration.zero,
+                  maxOpacity: 0.7,
+                ),
+                Glitters(
+                  minSize: 10.0,
+                  maxSize: 25.0,
+                  interval: Duration(milliseconds: 20),
+                  color: Colors.lime,
+                ),
+                Glitters(
+                  minSize: 10.0,
+                  maxSize: 25.0,
+                  duration: Duration(milliseconds: 200),
+                  inDuration: Duration(milliseconds: 500),
+                  outDuration: Duration(milliseconds: 500),
+                  interval: Duration(milliseconds: 30),
+                  color: Colors.white,
+                  maxOpacity: 0.7,
+                ),
+                Glitters(
+                  minSize: 14.0,
+                  maxSize: 30.0,
+                  interval: Duration(milliseconds: 40),
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+        ));
+
+   
   }
+  Widget roundedButton(String buttonLabel, Color bgColor, Color textColor) {
+    var loginBtn = new Container(
+      padding: EdgeInsets.all(5.0),
+      alignment: FractionalOffset.center,
+      decoration: new BoxDecoration(
+        color: bgColor,
+        borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: const Color(0xFF696969),
+            //    offset: Offset(1.0, 6.0),
+            // blurRadius: 0.001,
+          ),
+        ],
+      ),
+      child: Text(
+        buttonLabel,
+        style: new TextStyle(
+            color: textColor, fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+    );
+    return loginBtn;}
+
+  Future<bool> _onBackPressed() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Are you sure?'),
+            content: new Text('Do you want to exit an App'),
+            actions: <Widget>[
+              new GestureDetector(
+                onTap: () => Navigator.of(context).pop(false),
+                child:
+                    roundedButton("No", Colors.black, const Color(0xFFFFFFFF)),
+              ),
+              new GestureDetector(
+                onTap: () => Navigator.of(context).pop(true),
+                child: roundedButton(
+                    " Yes ", Colors.black, const Color(0xFFFFFFFF)),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
+  
+  final _flashOnController = TextEditingController(text: "Flash on");
+  final _flashOffController = TextEditingController(text: "Flash off");
+  final _cancelController = TextEditingController(text: "Cancel");
+
+  var _aspectTolerance = 0.00;
+  var _numberOfCameras = 0;
+  var _selectedCamera = -1;
+  var _useAutoFocus = true;
+  var _autoEnableFlash = false;
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  class Fair extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _FairState createState() => _FairState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FairState extends State<Fair> {
+  
+  File _image;
 
-  void _incrementCounter() {
+  Future getImage(ImgSource source) async {
+    var image = await ImagePickerGC.pickImage(
+        context: context,
+        source: source,
+        cameraIcon: Icon(
+          Icons.camera_alt,
+          color: Colors.red,
+        ), //cameraIcon and galleryIcon can change. If no icon provided default icon will be present
+        cameraText: Text(
+          "From Camera",
+          style: TextStyle(color: Colors.red),
+        ),
+        galleryText: Text(
+          "From Gallery",
+          style: TextStyle(color: Colors.blue),
+        ));
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _image = image;
     });
   }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+//SizedBox(child: Image.asset('assets/head.png'),)   ,
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                    child: Text(
+                  "Scan QR",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                )),
+                SizedBox(
+                  height: 20,
+                ),
+
+                InkWell(onTap: ()=>{//scan()
+                
+                },
+                                  child: SizedBox(
+                      child: Image.network(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzz4bOpap-wg5XHBdNPN1GKo_keqdt3aHFng&usqp=CAU')),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                    child: Text(
+                  "OR",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                )),
+                SizedBox(
+                  height: 20,
+                ),
+
+                Center(
+                    child: SizedBox(
+                        height: 90,
+                        width: 200,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            onTap: () => {
+                              getImage(ImgSource.Both),
+                            },
+                            child: Card(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Icon(Icons.camera_alt),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    "Take a photo",
+                                    style: TextStyle(fontSize: 20),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ))),
+                SizedBox(
+                  height: 10,
+                ),
+
+                Center(
+                  child: Text(
+                    "involve a picture for the bill ",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+
+                Center(
+                    child: SizedBox(
+                        height: 90,
+                        width: 200,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(onTap: ()=>{         
+Navigator.push(context,     MaterialPageRoute(builder: (context) => Page1()),
+)                          },
+                                                      child: Card(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                 
+                                  Text(
+                                    "Submit",
+                                    style: TextStyle(fontSize: 20),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ))),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: InkWell(
+                    onTap: ()=>{Navigator.push(context,     MaterialPageRoute(builder: (context) => Page2()),)
+},
+                                      child: Text(
+                      "Are you an inspector? If Yes, CLICK HERE  ",
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+  
 }
+
+
+
+
+
+  
+
